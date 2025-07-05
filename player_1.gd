744105d8bd60e75197297a89b330bed87bdf380b
Charlie Jansen <charliejansen2@gmail.com>
2025-07-04 18:50:55 -0700

    added player 2

--- player_1.gd
+++ player_1.gd
@@ -10,10 +10,10 @@ func _physics_process(delta):
 		if velocity.y > 600:
 			velocity.y = 600
 	
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	if Input.is_action_just_pressed("jump_p1") && is_on_floor():
 		velocity.y -= jump_force
 	
-	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	var horizontal_direction = Input.get_axis("move_left_p1", "move_right_p1")
 	
 	velocity.x = speed * horizontal_direction
 	

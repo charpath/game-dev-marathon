90728dc09fed43af17c57fda8a9e6337e39caf24
Cody Barriger <Meboy10000@gmail.com>
2025-07-04 20:11:08 -0700

    added gravity max property to players

--- player_2.gd
+++ player_2.gd
@@ -3,12 +3,13 @@ extends CharacterBody2D
 @export var speed = 300
 @export var gravity = 30
 @export var jump_force = 1000
+@export var grav_max = 600
 
 func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += gravity
-		if velocity.y > 600:
-			velocity.y = 600
+		if velocity.y > grav_max:
+			velocity.y = grav_max
 	
 	if Input.is_action_just_pressed("jump_p2") && is_on_floor():
 		velocity.y -= jump_force

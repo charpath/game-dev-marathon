ec0b8096ac4eb3901c811fa36b5dfd00c643a13e
Charlie Jansen <charliejansen2@gmail.com>
2025-07-04 20:42:41 -0700

    Added coyote frames to jumps

--- player_1.gd
+++ player_1.gd
@@ -5,15 +5,32 @@ extends CharacterBody2D
 @export var jump_force = 1000
 @export var grav_max = 600
 @export var player = 1
+@export var coyote_frames = 6
+var can_jump = true
+var cur_coyote = coyote_frames
+var on_ground = true
 
 func _physics_process(delta):
 	if !is_on_floor():
+		on_ground = false
 		velocity.y += gravity
 		if velocity.y > grav_max:
 			velocity.y = grav_max
+			
+		if cur_coyote <= 0:
+			can_jump = false
+		else:
+			cur_coyote -= 1
 	
-	if Input.is_action_just_pressed("jump_p{0}".format([player])) && is_on_floor():
-		velocity.y -= jump_force
+	if Input.is_action_pressed("jump_p{0}".format([player])) && can_jump:
+		can_jump = false
+		on_ground = false
+		velocity.y = -jump_force
+		
+	if !on_ground and is_on_floor():
+		cur_coyote = coyote_frames
+		can_jump = true
+		on_ground = true
 	
 	var horizontal_direction = Input.get_axis("move_left_p{0}".format([player]), "move_right_p{0}".format([player]))
 	

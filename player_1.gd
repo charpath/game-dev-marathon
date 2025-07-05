e37d977479c9cdbd3a813a31e2f2ca21b59314a7
Cody Barriger <Meboy10000@gmail.com>
2025-07-04 20:36:15 -0700

    unified players 1 and 2 to one script

--- player_1.gd
+++ player_1.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var gravity = 30
 @export var jump_force = 1000
 @export var grav_max = 600
+@export var player = 1
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -11,10 +12,10 @@ func _physics_process(delta):
 		if velocity.y > grav_max:
 			velocity.y = grav_max
 	
-	if Input.is_action_just_pressed("jump_p1") && is_on_floor():
+	if Input.is_action_just_pressed("jump_p{0}".format([player])) && is_on_floor():
 		velocity.y -= jump_force
 	
-	var horizontal_direction = Input.get_axis("move_left_p1", "move_right_p1")
+	var horizontal_direction = Input.get_axis("move_left_p{0}".format([player]), "move_right_p{0}".format([player]))
 	
 	velocity.x = speed * horizontal_direction
 	
